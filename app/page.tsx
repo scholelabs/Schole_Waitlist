d44ee29f7810b9e3432ea999d9100fff2c7c0554
lonnyrichard27 <lonnyrichard77@gmail.com>
2024-12-03 16:48:12 +0100

    added forms

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,40 @@ import { useState } from "react";
 
 export default function Home() {
   const [email, setEmail] = useState("");
+  const [school, setSchool] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const disabledBtn = !email || !name || !school || !phone
+
+  const handleSubmit = () => {
+      if(!email || !name || !school || !phone) {
+        return toast({
+          title: "Error",
+          description: "Please enter a valid email address",
+          variant: "destructive"
+        });
+      }
+
+      // const data = {email, phone, school, name}
+      joinWaitlist(email)
+        .then(() => {
+          setEmail("")
+          toast({
+            title: "Success",
+            description: "You have successfully joined the waitlist"
+          })
+        })
+        .catch(() =>
+          toast({
+            title: "Error",
+            description: "An error occurred while joining the waitlist",
+            variant: "destructive",
+          })
+        );
+  
+  }
+
   return (
     <main className="min-h-screen bg-black font-segoe text-white bg-custom-image bg-contain bg-left-bottom md:bg-left-bottom bg-no-repeat flex flex-col justify-between">
       <header className="flex justify-center py-4 mt-8">
@@ -36,6 +70,34 @@ export default function Home() {
         </div>
         <div className="flex flex-col items-center space-y-4 mt-10 w-full px-4">
           <div className="flex items-center space-x-2 bg-black  bg-opacity-50 border-white border rounded-md px-4 py-4 w-full max-w-md">
+            <input
+              type="text"
+              placeholder="Enter Name Of School"
+              className="bg-transparent text-white placeholder-gray- focus:outline-none flex-1"
+              value={school}
+              onChange={(e) => setSchool(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center space-x-2 bg-black  bg-opacity-50 border-white border rounded-md px-4 py-4 w-full max-w-md">
+            <input
+              type="text"
+              placeholder="Enter Your Name"
+              className="bg-transparent text-white placeholder-gray- pl focus:outline-none flex-1"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center space-x-2 bg-black  bg-opacity-50 border-white border rounded-md px-4 py-4 w-full max-w-md">
+            <span className="text-white"></span>
+            <input
+              type="number"
+              placeholder="Enter your phone number"
+              className="bg-transparent text-white placeholder-gray- pl focus:outline-none flex-1"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center space-x-2 bg-black  bg-opacity-50 border-white border rounded-md px-4 py-4 w-full max-w-md">
             <span className="text-white">@</span>
             <input
               type="email"
@@ -47,30 +109,8 @@ export default function Home() {
           </div>
           <button
             className="bg-teal-400 text-black rounded-full px-8 py-4"
-            onClick={() =>{
-              if(!email) {
-                return toast({
-                  title: "Error",
-                  description: "Please enter a valid email address",
-                  variant: "destructive",
-                });
-              }
-              joinWaitlist(email)
-                .then(() => {
-                  setEmail("")
-                  toast({
-                    title: "Success",
-                    description: "You have successfully joined the waitlist",
-                  })
-                })
-                .catch(() =>
-                  toast({
-                    title: "Error",
-                    description: "An error occurred while joining the waitlist",
-                    variant: "destructive",
-                  })
-                );
-            }}
+            disabled={disabledBtn}
+            onClick={handleSubmit}
           >
             Get Early Invitation
           </button>
